90313fcf99b9c4fe4592aeed97b26ee53cf1162d
Sandesh Pokhrel <sandesh.pokhrel56@gmail.com>
2017-11-09 16:50:29 +0545

    Committed to the git local

---
+++ src/main/java/com/sandesh/Greeting.java
@@ -0,0 +1,7 @@
+package com.sandesh;
+
+public final class Greeting {
+	public static String greet(String name) {
+		return "Warm welcome! " + name;
+	}
+}
---
+++ src/main/java/com/sandesh/Main.java
@@ -0,0 +1,9 @@
+package com.sandesh;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(Greeting.greet("Sandesh Pokhrel"));
+	}
+
+}
